1e43495f6c5b63e5d640039a549af32b0582621e
Gavlooth <Gavlooth@gmail.com>
2017-03-10 09:53:44 +0200

    simple weather app to retrive inforamtion from kos island weather station

---
+++ WeatherGadget/Gadget.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Windows;
+using System.ComponentModel;
+
+namespace WeatherGadget
+{
+
+    public class Gadget
+    {
+        static string Url = "http://penteli.meteo.gr/stations/kos/";                        
+        public string celsius;
+      
+        public Dictionary<string, string> DataScrap()
+            {
+                Dictionary<string, string> nodeDictionary = new Dictionary<string, string>();
+                HtmlWeb web = new HtmlWeb();
+                HtmlDocument htmlDoc = web.Load(Gadget.Url);          
+           foreach  (  var node in htmlDoc.DocumentNode.Descendants("font").Skip(15)) //Node number 16 contains the temperature
+           {              
+               nodeDictionary.Add("Temperature", (node.InnerText.Substring(0, node.InnerText.Length-2)));
+               break;
+           }        
+           
+           foreach  (  var node in htmlDoc.DocumentNode.Descendants("font").Skip(20)) //Node number 16 contains the temperature
+           {              
+               nodeDictionary.Add("Humidity",node.InnerText.Substring(0, node.InnerText.Length-2)+@" %");
+               break;
+           }
+                                  
+           foreach (var node in htmlDoc.DocumentNode.Descendants("font").Skip(28)) //Node number 16 contains the temperature
+           {
+               string tmp1, tmp2, tmp3;
+               tmp1 = tmp2 = tmp3 = node.InnerText;        
+       
+               tmp2 = tmp2.Remove(tmp2.IndexOf(" \r"));
+               tmp2 = tmp2.Substring(tmp2.IndexOf("at ")+3);   
+          
+               tmp1 = tmp1.Remove(tmp1.IndexOf("&")) + " Km/h";
+
+               tmp3 = tmp3.Remove(tmp3.IndexOf(")") - 1);
+               tmp3 = tmp3.Substring(tmp3.IndexOf("(") + 1);         
+
+               nodeDictionary.Add("Wind", tmp1+" at "+tmp2 );              
+               nodeDictionary.Add("WindAngle", tmp3);              
+               break;
+           }
+
+           foreach (var node in htmlDoc.DocumentNode.Descendants("font").Skip(34)) //Node number 16 contains the temperature
+           {
+               string tmp = node.InnerText;               
+               tmp = tmp.Replace("\r\n\t", ", ");             
+               nodeDictionary.Add("Barometer", tmp);                         
+               break;
+           }
+
+       
+           foreach (var node in htmlDoc.DocumentNode.Descendants("font").Skip(38)) //Node number 16 contains the temperature
+           {
+               string tmp1=node.InnerText;
+               tmp1 = tmp1.Remove(tmp1.IndexOf("&"));
+               nodeDictionary.Add("Today's Rain", tmp1+" mm");
+               break;
+           }
+           
+           foreach (var node in htmlDoc.DocumentNode.Descendants("font").Skip(44)) //Node number 16 contains the temperature
+           {
+              string tmp1;
+              tmp1  = node.InnerText;
+              tmp1 = tmp1.Remove(tmp1.IndexOf("&"));          
+              nodeDictionary.Add("Rain_rate", tmp1 + " mm/h");
+              break;
+           }           
+           foreach (var node in htmlDoc.DocumentNode.Descendants("font").Skip(48)) //Node number 16 contains the temperature
+           {       
+               string tmp1;
+               tmp1 = node.InnerText;
+               tmp1 = tmp1.Remove(tmp1.IndexOf("&"));                
+               nodeDictionary.Add("Storm_total", tmp1 + " mm");
+        
+               break;
+           }
+        
+           foreach (var node in htmlDoc.DocumentNode.Descendants("font").Skip(53)) //Node number 16 contains the temperature
+           {
+               string tmp1;
+               tmp1 = node.InnerText;
+               tmp1 = tmp1.Remove(tmp1.IndexOf("&"));                             
+               nodeDictionary.Add("Monthly_rain", tmp1 + " mm");
+               break;
+           }
+
+           foreach (var node in htmlDoc.DocumentNode.Descendants("font").Skip(62)) //Node number 16 contains the temperature
+           {
+               string tmp1;
+               tmp1 = node.InnerText;
+               tmp1 = tmp1.Remove(tmp1.IndexOf(" "));
+               nodeDictionary.Add("Wind_chill", tmp1 + " °C");             
+               break;
+           }
+
+           foreach (var node in htmlDoc.DocumentNode.Descendants("font").Skip(70)) //Node number 16 contains the temperature
+           {
+               string tmp1;
+               tmp1 = node.InnerText;
+               tmp1 = tmp1.Remove(tmp1.IndexOf("C")-1);
+               nodeDictionary.Add("Heat_index", tmp1 + " °C");               
+               break;
+             }
+
+           foreach (var node in htmlDoc.DocumentNode.Descendants("font").Skip(79)) //Node number 16 contains the temperature
+           {          
+
+               string tmp1;
+               tmp1 = node.InnerText;
+               tmp1 = tmp1.Remove(tmp1.IndexOf("&"));
+                              nodeDictionary.Add("Sunrise", tmp1 );
+
+             
+               break;
+           }
+
+           foreach (var node in htmlDoc.DocumentNode.Descendants("font").Skip(82)) //Node number 16 contains the temperature
+           {
+               string tmp1;
+               tmp1 = node.InnerText;
+               tmp1 = tmp1.Remove(tmp1.IndexOf("&"));
+               nodeDictionary.Add("Sunset", tmp1);
+               break;
+           }
+          
+           string temp1;
+           string temp2;
+           nodeDictionary.TryGetValue("Sunrise", out temp1);
+           nodeDictionary.TryGetValue("Sunset", out temp2);
+           string time = (TimeSpan.FromMinutes(Convert.ToDateTime("01/01/2015 " + temp2).Subtract(Convert.ToDateTime("01/01/2015 " + temp1)).TotalMinutes).ToString().Remove(5));
+           nodeDictionary.Add("Day_length", time);
+           return nodeDictionary;
+        
+           }
+    }
+
+}
---
+++ WeatherGadget/MainWindow.xaml.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.Drawing;
+using System.Threading;
+using System.ComponentModel;
+namespace WeatherGadget
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        public static void SetOnDesktop(Window window)
+        {
+            IntPtr hWnd = new System.Windows.Interop.WindowInteropHelper(window).Handle;
+            IntPtr hWndProgMan = FindWindow("Progman", "Program Manager");
+            SetParent(hWnd, hWndProgMan);
+        }
+
+        private Gadget skata = new Gadget();
+        private Dictionary<string, string> WeatherDictionary = new Dictionary<string, string>();
+        double _origin = 90;
+        static double zeroPoint = 135;
+        BackgroundWorker InBackround;
+
+        public void m_oWorker_DoWork(object sender, DoWorkEventArgs e)
+        {   while (true)
+            {
+
+              //  Thread.Sleep(60000);
+            InBackround.ReportProgress(0);
+            Thread.Sleep(600000);
+        }
+        }
+
+
+
+  
+
+private void MyWindow_Loaded(object sender, RoutedEventArgs e)
+{/*
+    try
+    {
+        SetOnDesktop(this.main_window);
+    }
+    catch (Exception pipa) { MessageBox.Show(pipa.ToString()); }
+   */ }
+
+
+
+
+        public void Update_Weather()
+        {
+         
+              
+                
+
+                lock (WeatherDictionary)
+                {
+                  
+
+                    string pipa;
+                    WeatherDictionary.TryGetValue("WindAngle", out pipa);
+                    SetCompass(_origin, 90 - Convert.ToDouble(pipa));
+
+                    WeatherDictionary = skata.DataScrap(); //retrive data from the weather station
+                    foreach (KeyValuePair<string, string> item in WeatherDictionary)
+                    {
+
+
+                        var temp = ((Label)this.FindName(item.Key.ToString()));
+                        if (temp == null)
+                            ;//MessageBox.Show("roufikten"); Do nothing
+                        else
+                        {
+                            lock (temp)
+                            {
+                                if (item.Key.ToString() == "Temperature")
+                                    temp.Content = item.Value.ToString() + " °C";
+                                else
+                                    temp.Content = (item.Key.ToString() == "Wind") ? item.Value.ToString() : item.Key.ToString().Replace("_", " ") + ": " + item.Value.ToString();
+                            }
+                        }
+                                             
+                   }
+         }
+        
+    }
+
+
+
+        public void SetCompass (double _origin,  double _destination)
+        {
+
+            _origin += zeroPoint;
+            _destination += zeroPoint;
+
+            var dirs = ( _destination - _origin < 360 + _origin - _destination) ? new[] {_origin, _destination} : new[] {_origin, _destination-360};
+
+
+             DoubleAnimation movePointer = new DoubleAnimation();
+             movePointer.From = dirs[0];
+             movePointer.To = dirs[1];
+
+            movePointer.Duration = new Duration(TimeSpan.FromSeconds(3));
+       
+            RotateTransform rt = new RotateTransform();
+            rt.CenterX =35 ;
+            rt.CenterY = 35;  
+            
+                pointer.RenderTransform = rt;
+                rt.BeginAnimation(RotateTransform.AngleProperty, movePointer);
+            
+            this._origin =   _destination - zeroPoint;
+
+        }
+
+
+        void m_oWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+         
+          Update_Weather();
+
+        }
+
+
+        void m_oWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //If it was cancelled midway
+            if (e.Cancelled)
+            {
+                MessageBox.Show ("Task Cancelled.");
+            }
+ 
+        }
+        
+        private void title_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        public MainWindow()
+        {
+            try
+            {
+
+               
+
+                #region GadgetTheme
+                this.ShowInTaskbar = false; //gadget doesnt display a taskbar
+                InitializeComponent();
+                X_Button.Opacity = 0; //the button x and tool beggin invisible
+                T_Button.Opacity = 0;
+                #endregion
+
+                #region initialize wind animation
+                pointer.Opacity = 0;
+                DoubleAnimation da = new DoubleAnimation();
+                da.From = 0;
+                da.To = zeroPoint+_origin;
+                da.Duration = new Duration(TimeSpan.FromSeconds(0.1));
+                //    da.RepeatBehavior = RepeatBehavior.Forever;
+                RotateTransform rt = new RotateTransform();
+                rt.CenterX = 35;
+                rt.CenterY = 35;
+                pointer.RenderTransform = rt;
+                rt.BeginAnimation(RotateTransform.AngleProperty, da);
+                pointer.Opacity = 1;
+                #endregion
+                                
+                #region initialize Weather data
+                WeatherDictionary = skata.DataScrap(); //retrive data from the weather station
+               foreach (KeyValuePair<string,string> item in WeatherDictionary)
+                {
+
+
+                    var temp = ((Label)this.FindName(item.Key.ToString()));
+                    if (temp == null)
+                        ;//MessageBox.Show("roufikten"); Do nothing
+                    else
+                    {
+                        if (item.Key.ToString() == "Temperature")
+                            temp.Content = item.Value.ToString() + " °C";
+                        else
+                        temp.Content = (item.Key.ToString() == "Wind") ? item.Value.ToString() : item.Key.ToString().Replace("_", " ") + ": " + item.Value.ToString();
+                    }
+                    
+                }
+
+
+    /*          string pipa;
+               WeatherDictionary.TryGetValue("WindAngle", out pipa);
+               SetCompass(_origin, Convert.ToDouble(pipa));
+    */
+                #endregion
+
+               InBackround = new BackgroundWorker();
+               InBackround.DoWork += new DoWorkEventHandler(m_oWorker_DoWork);
+               InBackround.ProgressChanged += new ProgressChangedEventHandler(m_oWorker_ProgressChanged);
+               InBackround.RunWorkerCompleted += new RunWorkerCompletedEventHandler(m_oWorker_RunWorkerCompleted);
+               InBackround.WorkerReportsProgress = true;
+               InBackround.WorkerSupportsCancellation = false;
+
+               InBackround.RunWorkerAsync();
+           
+               
+            }
+            catch (Exception e) { MessageBox.Show(e.ToString()); }
+        }
+
+
+        #region Button Style
+        public void ReduceOpacity(object sender, EventArgs e)
+        {
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = X_Button.Opacity;
+            doubleAnimation.To = 0;
+            doubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            this.X_Button.BeginAnimation(OpacityProperty, doubleAnimation);
+            this.T_Button.BeginAnimation(OpacityProperty, doubleAnimation);
+
+        }
+
+
+        public void EnhanceOpacity(object sender, EventArgs e)
+        {
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = X_Button.Opacity;
+            doubleAnimation.To = 1;
+            doubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            this.X_Button.BeginAnimation(OpacityProperty, doubleAnimation);
+            this.T_Button.BeginAnimation(OpacityProperty, doubleAnimation);
+
+        }
+
+        #endregion
+
+   
+
+    }
+     
+ }
+
